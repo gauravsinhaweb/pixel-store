d13cc2fe52971d47b7b20bf32ae22bf48fd68b64
gauravsinhaweb <sinhagaaurav37@gmail.com>
2022-04-06 16:53:59 +0530

    Update Hero.css

--- src/pages/hero/Hero.jsx
+++ src/pages/hero/Hero.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Link as LinkToID } from "react-scroll";
 import { Footer, Navbar } from "../../components/components-index.js";
-import "./hero.css";
+import "./Hero.css";
 import { HeroDisplay } from "./HeroDisplay.jsx";
 
 export const Hero = (props) => {
