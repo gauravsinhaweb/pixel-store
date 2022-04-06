d9191b62d23e073ef0507f77415ea8f570b86816
gauravsinhaweb <sinhagaaurav37@gmail.com>
2022-04-07 03:11:40 +0530

    Update: footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,35 +5,28 @@ export function Footer() {
     <footer className="home_footer flex">
       <div>
         <ul className="p footer_ul">
-          <li className="p-lg">category</li>
-          <li>Ea labore officia</li>
-          <li>Ea labore officia</li>
-          <li>Ea labore officia</li>
-          <li>Ea labore officia</li>
-          <li>Ea labore officia</li>
-          <li>Ea labore officia</li>
+          <li className="p-lg">CONTACT</li>
+          <li>35 W 46nd Street, Wall Road New York, USA.</li>
+          <li>+(000) 1234-56789</li>
+          <li>sinhagaurav.me@gmail.com</li>
         </ul>
       </div>
       <div>
         <ul className="p footer_ul">
-          <li className="p-lg">Country</li>
-          <li>Ea labore officia</li>
-          <li>Ea labore officia</li>
-          <li>Ea labore officia</li>
-          <li>Ea labore officia</li>
-          <li>Ea labore officia</li>
-          <li>Ea labore officia</li>
+          <li className="p-lg">SUPPORT</li>
+          <li>Faq</li>
+          <li>Shipping & Returns</li>
+          <li>Contact Us</li>
+          <li>Our Partners</li>
         </ul>
       </div>
       <div>
         <ul className="p footer_ul">
-          <li className="p-lg">About Us</li>
-          <li>Ea labore officia</li>
-          <li>Ea labore officia</li>
-          <li>Ea labore officia</li>
-          <li>Ea labore officia</li>
-          <li>Ea labore officia</li>
-          <li>Ea labore officia</li>
+          <li className="p-lg">INFO</li>
+          <li>About Us</li>
+          <li>Our Stores</li>
+          <li>Size Guide</li>
+          <li>Our Piercing Service</li>
         </ul>
       </div>
     </footer>
